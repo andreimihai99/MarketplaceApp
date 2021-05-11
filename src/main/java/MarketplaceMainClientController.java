7e9ad28303011fb6e4ad911578ead841dfe079eb
Andrei Mihai <andreimihai99@yahoo.com>
2021-05-11 16:16:26 +0300

    Added GUI for manager use case

--- src/main/java/MarketplaceMainClientController.java
+++ src/main/java/MarketplaceMainClientController.java
@@ -1,2 +1,41 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class MarketplaceMainClientController {
+
+    @FXML
+    private TextField searchProductField;
+
+    @FXML
+    private TextField productQuantityField;
+
+    @FXML
+    private Label availabilityLabel;
+
+    private NavigatePages nav = new NavigatePages();
+
+    @FXML
+    void addToBasketButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToRegisterButtonAction(ActionEvent event) throws IOException {
+        nav.changeToPage(event, "src/main/resources/Scenes/RegisterClient.fxml");
+    }
+
+    @FXML
+    void seeProductsButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkAvailabilityButtonAction(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/ModifyProductsController.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ModifyProductsController {
+
+    @FXML
+    private TextField modifiedProductName;
+
+    @FXML
+    private TextField newQuantityField;
+
+    @FXML
+    private TextField newPriceField;
+
+    @FXML
+    private Label productDetailsLabel;
+
+    @FXML
+    void checkProductDetailsButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void makeModificationsButtonAction(ActionEvent event) {
+
+    }
+
+}
+
